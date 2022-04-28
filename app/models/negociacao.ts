c5e1678353bae79e41a6893fec197aa2f67875fa
thefjrm <felipemartinsplayernet@gmail.com>
2022-04-28 16:31:55 -0300

    Outros arquivos

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,31 +1,16 @@
 export class Negociacao {
-  // Devemos passar os tipos também no construtor das classes.
-  constructor(
-    private _data: Date,
-    private _quantidade: number,
-    private _valor: number
-  ) {}
-  // Podemos passsar as propriedades da classe diretamente como parâmetros do construtor, sem precisar declará-las antes.
+    constructor(
+        private _data: Date, 
+        public readonly quantidade: number, 
+        public readonly valor: number
+    ) {}
 
-  get data(): Date {
-    const data = new Date(this._data.getTime()); // Criando uma nova data para não alterar a data original, pois mesmo setando a propriedade como não modificável, por uma brecha do método setTime() do objeto Date, a data pode ser modificada.
-    return this._data;
-  }
+    get volume(): number {
+        return this.quantidade * this.valor;
+    }
 
-  get quantidade(): number {
-    return this._quantidade;
-  }
-
-  get valor(): number {
-    return this._valor;
-  }
-
-  // Ao invés de passar os getters para receber as propriedades privadas de uma classe, podemos declarar as propriedades como públicas e passar o atributo readonly evitando o uso dos getters. Devemos deixar só o de volume, que no caso ficaria:
-  // public readonly data: Date,
-  // public readonly quantidade: number,
-  // public readonly valor: number,
-
-  get volume(): number {
-    return this._quantidade * this._valor;
-  }
+    get data(): Date {
+        const data = new Date(this._data.getTime());
+        return data;
+    }
 }
--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,15 +1,13 @@
-import { Negociacao } from "./negociacao.js";
+import { Negociacao } from './negociacao.js';
 
 export class Negociacoes {
-  // Quando definimos uma array, devemos definir também o tipo da array. Entre tags colocamos o que a array vai receber. Neste caso a lista somente receberá elementos do tipo da classe Negociacao.
-  private negociacoes: Array<Negociacao> = []; // Negociacao[] funciona do mesmo jeito sendo um atalho.
+    private negociacoes: Negociacao[] = [];
 
-  adiciona(negociacao: Negociacao) {
-    this.negociacoes.push(negociacao);
-  }
-  // Neste caso de somente leitura fazemos readonly Negociacao[].
-  lista(): ReadonlyArray<Negociacao> {
-    // Com o tipo ReadonlyArray, não podemos alterar o array. Somente leitura.
-    return this.negociacoes;
-  }
+    adiciona(negociacao: Negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+
+    lista(): readonly Negociacao[] {
+        return this.negociacoes;
+    }
 }
