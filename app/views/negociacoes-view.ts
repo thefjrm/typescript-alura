2363b9664b3ccf03f9ad63c01749d2818ec15449
thefjrm <felipemartinsplayernet@gmail.com>
2022-04-28 18:36:22 -0300

    Mudanças de métodos para private e enum

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -2,7 +2,7 @@ import { Negociacoes } from "../models/negociacoes.js";
 import { View } from "./view.js";
 
 export class NegociacoesView extends View<Negociacoes> {
-  template(model: Negociacoes): string {
+  protected template(model: Negociacoes): string {
     return `
         <table class="table table-hover table-bordered">
             <thead>
@@ -18,9 +18,7 @@ export class NegociacoesView extends View<Negociacoes> {
                   .map((negociacao) => {
                     return `
                         <tr>
-                            <td>${new Intl.DateTimeFormat().format(
-                              negociacao.data
-                            )}
+                            <td>${this.formatar(negociacao.data)}</td>
                             </td>
                             <td>
                                 ${negociacao.quantidade}
@@ -35,5 +33,9 @@ export class NegociacoesView extends View<Negociacoes> {
             </tbody>
         </table>
         `;
+  }
+
+  private formatar(data: Date): string {
+    return new Intl.DateTimeFormat().format(data);
   }
 }
