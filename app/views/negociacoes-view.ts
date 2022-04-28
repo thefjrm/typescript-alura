7659a0540c35db61d0902d8e3784e71fa485ede5
thefjrm <felipemartinsplayernet@gmail.com>
2022-04-28 17:06:00 -0300

    Tabela com negociações

---
+++ app/views/negociacoes-view.ts
@@ -0,0 +1,51 @@
+import { Negociacoes } from "../models/negociacoes.js";
+
+export class NegociacoesView {
+  private elemento: HTMLElement;
+
+  constructor(seletor: string) {
+    this.elemento = document.querySelector(seletor);
+  }
+
+  template(model: Negociacoes): string {
+    return `
+    <table class="table table-hover table-bordered">
+      <thead>
+        <tr>
+          <th>DATA</th>
+          <th>QUANTIDADE</th>
+          <th>VALOR</th>
+          <th>VOLUME</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        ${model
+          .lista()
+          .map((negociacao) => {
+            return `
+            <tr>
+              <td>${negociacao.data}</td>
+              <td>${negociacao.quantidade}</td>
+              <td>${negociacao.valor}</td>
+            </tr>
+          `;
+          })
+          .join("")}
+      </tbody>
+
+      <tfoot>
+        <td colspan="3"></td>
+        <td>
+          <button class="btn btn-primary" id="importar">Importar</button>
+        </td>
+      </tfoot>
+    </table>
+    `;
+  }
+
+  update(model: Negociacoes): void {
+    const template = this.template(model);
+    this.elemento.innerHTML = template;
+  }
+}
